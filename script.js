bf7ba37f15d4a00ac12c49a32c0991e00ff664e3
keysman59 <keysman.dima@yandex.ru>
2020-04-30 15:25:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/* /* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// function plusSlide() {
+//     showSlides(slideIndex += 1);
+// }
+
+// function minusSlide() {
+//     showSlides(slideIndex -= 1);  
+// }
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
